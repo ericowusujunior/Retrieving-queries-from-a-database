e13bb901404b32546457eeec759615093ea71f42
Eric Owusu Junior <e.owusu89@gmail.com>
2022-02-22 02:41:21 +0000

    used the 3rd party library pandas to do sql query and create/save query into a csv file

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import sqlite3
+import pandas
 
 #Establish connection and create cursor
 conn = sqlite3.connect('database.db')
 cur = conn.cursor()
 
 #Execute SQL
-cur.execute("SELECT * FROM ips WHERE asn==198 ORDER BY asn")
-print(cur.fetchall())
+#cur.execute("SELECT * FROM ips WHERE asn==198 ORDER BY asn")
+#print(cur.fetchall())
+
+#Converting SQl to CSV
+df = pandas.read_sql_query("SELECT * FROM ips", conn)
+print(df)
+
+df.to_csv("database1", index = None)
